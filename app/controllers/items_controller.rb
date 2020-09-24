60283be39d57194f17cb0839672048a3e93621c7
kentaroupstream <arc.hang.ski@gmail.com>
2020-09-24 19:50:59 +0900

    商品編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,10 +16,20 @@ class ItemsController < ApplicationController
       redirect_to root_path
     else
       render :new
-    end
+    end  
   end
 
   def show
+  end
+
+  def edit
+    redirect_to root_path if current_user.id != @item.user_id
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to root_path
   end
 
   private
